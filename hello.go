059e3cdf994757f856cd8ea5f714292bfbcb2e08
Prudhvi Rampey <prudhvi.rampey@gmail.com>
2021-05-30 18:44:57 +0530

    Added simple web server

--- hello.go
+++ hello.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, Koen"))
+}
 
 func main() {
-	fmt.Println("Hello, Koen")
+	router := mux.NewRouter()
+	router.HandleFunc("/", HomeHandler)
+	http.Handle("/", router)
+
+	// Our application will run on port 8080. Here we declare the port and pass in our router.
+	http.ListenAndServe(":8080", router)
+
 }
