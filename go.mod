3bffa3da11d0bf2aa715af6d7760f2236f04b0ee
Prudhvi Rampey <prudhvi.rampey@gmail.com>
2021-05-30 22:55:36 +0530

    Serving static files

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module bitbucket.org/cryptopatron/backend
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/go-chi/chi/v5 v5.0.3
+)
--- hello.go
+++ hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -21,7 +22,43 @@ func main() {
 	router.Get("/", HomeHandler)
 	router.Get("/login", LoginHandler)
 
+	webAppPath := "../front-end/dist/dropcoin/"
+	setupFileServer(router, webAppPath)
+
 	// Our application will run on port 8080. Here we declare the port and pass in our router.
 	http.ListenAndServe(":8080", router)
 
+}
+
+// FileServer conveniently sets up a http.FileServer handler to serve
+// static files from a http.FileSystem.
+// func FileServer(r chi.Router, path string, root http.FileSystem) {
+// 	if strings.ContainsAny(path, "{}*") {
+// 		panic("FileServer does not permit any URL parameters.")
+// 	}
+
+// 	if path != "/" && path[len(path)-1] != '/' {
+// 		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+// 		path += "/"
+// 	}
+// 	path += "*"
+
+// 	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+// 		rctx := chi.RouteContext(r.Context())
+// 		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+// 		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
+// 		fs.ServeHTTP(w, r)
+// 	})
+// }
+
+func setupFileServer(router *chi.Mux, root string) {
+	fs := http.FileServer(http.Dir(root))
+
+	router.Get("/*", func(w http.ResponseWriter, r *http.Request) {
+		if _, err := os.Stat(root + r.RequestURI); os.IsNotExist(err) {
+			http.StripPrefix(r.RequestURI, fs).ServeHTTP(w, r)
+		} else {
+			fs.ServeHTTP(w, r)
+		}
+	})
 }
