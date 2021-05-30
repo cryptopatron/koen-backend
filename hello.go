5df10222a0938b0f9e8c13e2bfdf2ee142b6af5e
Prudhvi Rampey <prudhvi.rampey@gmail.com>
2021-05-30 22:16:17 +0530

    Switched to a faster router 'chi'

--- hello.go
+++ hello.go
@@ -3,17 +3,23 @@ package main
 import (
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, Koen"))
 }
 
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Logincurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash"))
+}
+
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/", HomeHandler)
-	http.Handle("/", router)
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/", HomeHandler)
+	router.Get("/login", LoginHandler)
 
 	// Our application will run on port 8080. Here we declare the port and pass in our router.
 	http.ListenAndServe(":8080", router)
